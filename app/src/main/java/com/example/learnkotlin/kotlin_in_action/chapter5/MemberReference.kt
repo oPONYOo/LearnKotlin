39c7cd04691d1720a9b3f8484859669f78d63521
rlawlsdk.1127 <rlawlsdk.1127@daum.net>
2023-02-18 02:47:03 +0900

    keyword: constructor reference, bound member reference

--- app/src/main/java/com/example/learnkotlin/kotlin_in_action/chapter5/MemberReference.kt
+++ app/src/main/java/com/example/learnkotlin/kotlin_in_action/chapter5/MemberReference.kt
@@ -8,8 +8,50 @@ val getAge = Person::age // 클래스::멤버
 
 fun salute() = println("salute!") // 최상위 함수
 
+fun sendEmail(person: Person, message: String) {}
+
+val action = {person: Person, message: String -> sendEmail(person, message) }
+val nextAction = ::sendEmail
 
 
+// constructor reference(생성자 참)
+val createPerson = ::Person
+val p = createPerson("JINA", 24)
+
+// 확장 함수 참조도 가능
+fun Person.isAdult() = age >= 20
+val predicate = Person::isAdult
+
+
+/*public static final boolean isAdult(@NotNull Person $this$isAdult) {
+    Intrinsics.checkNotNullParameter($this$isAdult, "$this$isAdult");
+    return $this$isAdult.getAge() >= 20;
+
+     @NotNull
+   public static final KFunction getPredicate() {
+      return predicate;
+   }
+}*/
 fun main() {
+    println(getAge(Person("jina", 24)))
     run(::salute) // 최상위 함수 참조. 최상위 함수이므로 ::앞에 클래스 이름을 생략한다.
+
+    println(p)
+
+    // bound member reference
+    val jinaAgeFunction = p::isAdult
+    /*val jinaAgeFunction: KFunction = object : Function0(MemberReferenceKt.p) {
+        // $FF: synthetic method
+        // $FF: bridge method
+        operator fun invoke(): Any? {
+            return this.invoke()
+        }
+
+        // 인스턴스에 대해 멤버를 호출
+        operator fun invoke(): Boolean {
+            return MemberReferenceKt.isAdult(this.receiver as Person?)
+        }
+    }*/
+    println(jinaAgeFunction())
+    println(predicate(p))
 }
