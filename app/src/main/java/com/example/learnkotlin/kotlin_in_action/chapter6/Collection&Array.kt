2ac661e0fe022df358e174358f85f661c6ef4551
rlawlsdk.1127 <rlawlsdk.1127@daum.net>
2023-02-25 00:57:51 +0900

    keyword: Collection, mutableCollection, Array, IntArray

---
+++ app/src/main/java/com/example/learnkotlin/kotlin_in_action/chapter6/Collection&Array.kt
@@ -0,0 +1,25 @@
+package com.example.learnkotlin.kotlin_in_action.chapter6
+
+
+fun main() {
+    val map = mapOf<String, Int>()
+    val letters = Array<String>(26) { i -> ('a' + i).toString() } // 타입 명시 안해도 컴파일러가 자동으로 원소 타입 추론해줌
+    println(letters.joinToString(""))
+
+    val strings = listOf("a", "b", "c")
+    println("%s/%s/%s".format(*strings.toTypedArray()))
+    // vararg 인자를 넘기기 위해 스프레드 연산자 사용
+
+    val fourOnes = IntArray(4)
+    val fourOnesToo = intArrayOf(1, 1, 1, 1) // IntArray생성
+
+    val squares = IntArray(5) { i -> (i + 1) * (i + 1) }
+    println(squares.joinToString())
+
+    fun array(args: Array<String>) {
+        args.forEachIndexed { index, element ->
+            println("Argument $index is : $element")
+        }
+    }
+    array(Array(5) { "jina" })
+}
