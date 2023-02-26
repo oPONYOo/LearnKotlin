8291fe65c8993c2aee3514fddb26fbc900961825
rlawlsdk.1127 <rlawlsdk.1127@daum.net>
2023-02-26 23:38:24 +0900

    keyword: delegation property, backing property ,by lazy

---
+++ app/src/main/java/com/example/learnkotlin/kotlin_in_action/chapter7/DelegatedPropertyEx1.kt
@@ -0,0 +1,55 @@
+package com.example.learnkotlin.kotlin_in_action.chapter7
+
+class Email {
+
+}
+
+// backing property 기법
+// thread - safe 하지않음
+class BackingPropertyPerson(val name: String) {
+    private var _emails: List<Email>? = null // emails의 위임 객체 역할. 데이터를 저장한다.
+    val emails: List<Email> // _emails라는 프로퍼티에 대한 읽기 연산 제공
+    get() {
+        if (_emails == null) _emails = loadEmails(this) // 최조 접근 시 이메일을 가져온다.
+        return _emails!! // 저장해둔 데이터가 있으면 해당 데이터 반환
+    }
+
+}
+
+fun loadEmails(person: BackingPropertyPerson): List<Email> {
+    println("Load emails for ${person.name}")
+    return listOf()
+}
+
+// 위임 프로퍼티는 데이터를 저장할 때 쓰이는 backing property와
+// 값이 오직 한 번만 초기화됨을 보장하는 게터 로직을 함께 캡슐화 해준다.
+class ByLazyPerson(val name: String) {
+    val emails by lazy { loadEmails(this) }
+    // lazy 함수의 인자는 값을 초기화할 때 호출할 람다
+
+    /*@NotNull
+    public final List getEmails() {
+        Lazy var1 = this.emails$delegate;
+        Object var3 = null;
+        return (List)var1.getValue();
+    }*/
+}
+
+//public actual fun <T> lazy(initializer: () -> T): Lazy<T> = SynchronizedLazyImpl(initializer)
+
+
+fun loadEmails(person: ByLazyPerson): List<Email> {
+    println("Load emails for ${person.name}")
+    return listOf()
+}
+
+fun main() {
+
+    val p = BackingPropertyPerson("Jina")
+    p.emails // 최초로 emails를 읽을 때 단 한번만 이메일을 가져온다.
+    p.emails
+
+    val p2 = ByLazyPerson("ponyo")
+    p2.emails
+    p2.emails
+}
